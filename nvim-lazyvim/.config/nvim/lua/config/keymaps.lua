5f551c5e3004dcd947e1f236a1b7d7654c6b32dc
Shubhang Mani <shubhang@trustcloud.ai>
2025-05-21 10:58:19 -0500

    Plugin updates and keymap to copy active file path to clipboard

--- nvim-lazyvim/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>yp", function()
+  vim.fn.setreg(vim.v.register, vim.fn.expand("%:p"))
+end, { desc = "Copy filename to clipboard" })
